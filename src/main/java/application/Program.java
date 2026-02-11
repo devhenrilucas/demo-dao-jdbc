ed68e555d92253e17ad8d42ecb5fcc7be0ac7085
devhenrilucas <lucazsbs15@gmail.com>
2026-02-11 16:53:10 -0300

    SellerDaoJDBC and DaoFactory

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
         Department dt = new Department(1, "Police");
 
         Seller seller = new Seller(21, "Lucas", "lucaz@gmail.com", new Date(), 1000.00, dt);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
 
