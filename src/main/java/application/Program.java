a8a0f15bd131db1776b11d7a5d449cf5e39ab08b
devhenrilucas <lucazsbs15@gmail.com>
2026-02-11 19:26:07 -0300

    Insert implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -26,12 +26,18 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("=== Teste numero 2: Seller findAll =====");
+        System.out.println("=== Teste numero 3: Seller findAll =====");
 
         list = sellerDao.findAll();
 
         for (Seller obj: list) {
             System.out.println(obj);
         }
+
+        System.out.println("=== Teste numero 4: Seller insert =====");
+        Seller newSeller = new Seller(null, "Greg","greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
+        
     }
 }
