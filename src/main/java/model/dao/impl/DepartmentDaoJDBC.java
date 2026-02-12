f48a8cb8d0f090a2d528b1fa123431ee24279250
devhenrilucas <lucazsbs15@gmail.com>
2026-02-12 04:08:13 -0300

    DepartmentDao implementation and test

---
+++ src/main/java/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,48 @@
+package model.dao.impl;
+
+import db.DB;
+import db.DbException;
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    private Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+
+    @Override
+    public void insert(Department obj) {
+
+    }
+
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+       return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return List.of();
+    }
+}
